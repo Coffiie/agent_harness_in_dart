1661caa66c3a31688b21bc9b9dd8ce6d5c57cbed
Coffiie <54104668+Coffiie@users.noreply.github.com>
2026-07-08 21:04:42 +0500

    feat: add chat loop

--- lib/coding_agent_in_dart.dart
+++ lib/coding_agent_in_dart.dart
@@ -2,20 +2,32 @@ import 'dart:io';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
 
+String divider() {
+  return '-' * 20;
+}
+
 String input() {
-  return stdin.readLineSync() ?? '';
+  stdout.write('${divider()}\n');
+  stdout.write('You: ');
+  final input = stdin.readLineSync() ?? '';
+  stdout.write('${divider()}\n');
+  return input;
 }
 
 void main(List<String> arguments) async {
   final modelName = 'gemini-2.5-flash';
+  print(divider());
+  print('Chat with $modelName: (press ctrl-c to exit)');
+  print(divider());
 
-  print('Chat with $modelName:');
-  final prompt = input();
+  while (true) {
+    final prompt = input();
 
-  callModel(modelName, prompt);
+    await callModel(modelName, prompt);
+  }
 }
 
-void callModel(String modelName, String prompt) async {
+Future<void> callModel(String modelName, String prompt) async {
   // fetch api key from environment variable setup in the terminal
   final apiKey = Platform.environment['GEMINI_API_KEY'] ?? '';
 
