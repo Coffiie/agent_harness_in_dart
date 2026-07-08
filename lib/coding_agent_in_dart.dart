594bb151750af1b5a7fd912a71a5c0cdffd484e1
Coffiie <54104668+Coffiie@users.noreply.github.com>
2026-07-08 11:06:31 +0500

    feat: take input from user

--- lib/coding_agent_in_dart.dart
+++ lib/coding_agent_in_dart.dart
@@ -2,7 +2,20 @@ import 'dart:io';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
 
+String input() {
+  return stdin.readLineSync() ?? '';
+}
+
 void main(List<String> arguments) async {
+  final modelName = 'gemini-2.5-flash';
+
+  print('Chat with $modelName:');
+  final prompt = input();
+
+  callModel(modelName, prompt);
+}
+
+void callModel(String modelName, String prompt) async {
   // fetch api key from environment variable setup in the terminal
   final apiKey = Platform.environment['GEMINI_API_KEY'] ?? '';
 
@@ -12,15 +25,15 @@ void main(List<String> arguments) async {
   }
 
   // create a model instance
-  final model = GenerativeModel(model: 'gemini-2.5-flash', apiKey: apiKey);
+  final model = GenerativeModel(model: modelName, apiKey: apiKey);
 
   try {
     // create a content instance
-    final content = [Content.text('What is the capital of France?')];
+    final content = [Content.text(prompt)];
     final response = await model.generateContent(content);
 
     // print the response
-    print(response.text);
+    print("$modelName: ${response.text}");
   } catch (e) {
     print('Error generating content: $e');
   }
