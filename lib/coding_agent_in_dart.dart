4cbba8bb9aa183db30a2a51b5142386637ddae40
Coffiie <54104668+Coffiie@users.noreply.github.com>
2026-07-18 19:39:38 +0500

    feat: Added readFile tool call

--- lib/coding_agent_in_dart.dart
+++ lib/coding_agent_in_dart.dart
@@ -15,26 +15,6 @@ String input() {
 }
 
 void main(List<String> arguments) async {
-  final conversation = <String>[];
-
-  final modelName = 'gemini-3.1-flash-lite';
-  print(divider());
-  print('Chat with $modelName: (press ctrl-c to exit)');
-  print(divider());
-
-  while (true) {
-    final prompt = input();
-    conversation.add("You: $prompt");
-
-    await callModel(modelName, prompt, conversation);
-  }
-}
-
-Future<void> callModel(
-  String modelName,
-  String prompt,
-  List<String> conversation,
-) async {
   // fetch api key from environment variable setup in the terminal
   final apiKey = Platform.environment['GEMINI_API_KEY'] ?? '';
 
@@ -43,22 +23,85 @@ Future<void> callModel(
     return;
   }
 
+  final conversation = <Content>[];
+  final toolResults = <FunctionResponse>[];
+
+  //create the function & tool for read_file
+  final readFileFunctionDec = FunctionDeclaration(
+    'readFile',
+    'Read the contents of a file from a given path',
+    Schema.string(description: 'the path of the file', nullable: false),
+  );
+  final tools = [
+    Tool(functionDeclarations: [readFileFunctionDec]),
+  ];
+
+  final modelName = 'gemini-3.1-flash-lite';
+  print(divider());
+  print('Chat with $modelName: (press ctrl-c to exit)');
+  print(divider());
+
   // create a model instance
-  final model = GenerativeModel(model: modelName, apiKey: apiKey);
+  final model = GenerativeModel(model: modelName, apiKey: apiKey, tools: tools);
 
+  while (true) {
+    final prompt = input();
+    conversation.add(Content('user', [TextPart(prompt)]));
+
+    await callModel(model, modelName, conversation, tools, toolResults);
+  }
+}
+
+Future<void> callModel(
+  GenerativeModel model,
+  String modelName,
+  List<Content> conversation,
+  List<Tool> tools,
+  List<FunctionResponse> toolResults,
+) async {
   try {
-    // create a content instance
-    final content = conversation.map((c) => Content.text(c)).toList();
-    final response = await model.generateContent(content);
+    while (true) {
+      final response = await model.generateContent(conversation);
 
-    final responseWithModelName = "$modelName: ${response.text}";
+      // continue same flow if model did not ask to use a tool
+      if (response.text?.isNotEmpty ?? false) {
+        // print the response
+        print('$modelName: ${response.text}');
+        conversation.add(Content('model', [TextPart(response.text!)]));
+        break;
+      }
 
-    // print the response
-    print(responseWithModelName);
-
-    //add response to chat history
-    conversation.add(responseWithModelName);
+      // loop through model's suggested tool calls and call those tools
+      for (var function in response.functionCalls) {
+        if (function.name == 'readFile') {
+          //call readFile
+          final response = await readFile(function.args['path'].toString());
+          conversation.add(Content('user', [response]));
+          continue;
+        }
+      }
+    }
   } catch (e) {
     print('Error generating content: $e');
+  }
+}
+
+//reads file given a path
+Future<FunctionResponse> readFile(String path) async {
+  print('Called readFile');
+
+  // Create a reference to the file location
+  final file = File(path);
+
+  final toolName = 'readFile';
+
+  try {
+    // Read the full file contents asynchronously
+    String contents = await file.readAsString();
+
+    return FunctionResponse(toolName, {'content': contents});
+  } catch (e) {
+    print('Error reading file: $e');
+    return FunctionResponse(toolName, {'error': e.toString()});
   }
 }
